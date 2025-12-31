4ba51d008b3d9aa14cb815461467e71de9060ffd
Devender Mahajan <devendermahajan@gmail.com>
2025-12-31 14:57:50 +0530

    a changes made

--- newfile.py
+++ newfile.py
@@ -1,9 +1,5 @@
 l=[23,43,54,54,223,1,2,43,54,65,23]
-# removing duplicate using loops
-u = []
-for v in l:
-    if v not in u:
-        u.append(v)
-
+# removing duplicate using dictionary
+u = list(dict.fromkeys(l))
 print("Original list:",l)
 print("Unique list:",u)
